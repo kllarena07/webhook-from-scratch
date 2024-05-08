44782bfec67f2a13d9a437881e1da0ca7ef57497
Kieran Llarena <baymax709@gmail.com>
2024-05-08 12:57:24 -0400

    feat(frontend): express.js ssr

---
+++ frontend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const staticDir = path.join(__dirname, 'src');
+
+app.use(express.static(staticDir));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
