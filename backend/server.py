947c1881103996df415ed536041c0e1c178b58a1
Kieran Llarena <baymax709@gmail.com>
2024-05-08 12:21:22 -0400

    feat(backend): init basic webhook server architecture

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,25 @@
-from flask import Flask
+import json
+from flask import Flask, jsonify, request
+import threading
+import time
 
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return "Hello, World!"
+def print_args(request_json):
+    time.sleep(3)
+    
+    content = json.loads(request_json)
+    print(content['name'])
 
-if __name__ == "__main__":
+@app.route('/', methods=['POST'])
+def index():
+    print("Successfully recieved request.")
+
+    print("Starting task.")
+    content = json.dumps(request.json)
+    threading.Thread(target=print_args, args=(content,)).start()
+
+    return jsonify({"message": "Accepted"}), 202
+
+if __name__ == '__main__':
     app.run(port=8080, debug=True)
