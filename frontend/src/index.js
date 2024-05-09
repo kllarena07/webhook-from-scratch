d79dc9bab6eba444c9c5642eb2d3cb8fd943c6c4
Kieran Llarena <baymax709@gmail.com>
2024-05-09 10:37:42 -0400

    refactor(frontend): better random id naming

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,16 @@
+let id = null;
+
 window.addEventListener('load', async () => {
   const response = await fetch('http://127.0.0.1:3000/register')
   const data = await response.json();
 
-  console.log(data);
+  id = data.id;
 
-  const event_source = new EventSource("http://127.0.0.1:3000/webhook/call_back");
+  // const event_source = new EventSource("http://127.0.0.1:3000/webhook/call_back");
 
-  event_source.addEventListener("message", (e) => {
-    console.log(e);
-  });
+  // event_source.addEventListener("message", (e) => {
+  //   console.log(e);
+  // });
 });
 
 const button = document.querySelector('button');
@@ -16,7 +18,8 @@ const pre = document.querySelector('pre');
 
 button.addEventListener("click", async () => {
   const data = {
-    "webhook_endpoint": "http://127.0.0.1:3000/webhook/call_back"
+    "webhook_endpoint": "http://127.0.0.1:3000/webhook/call_back",
+    "id": id
   };
 
   const response = await fetch("http://127.0.0.1:8080/", {
